9761641b0b6435af7e429083afb6e2844ea3668b
Jose Carlos Mena <jcme0003@red.ujaen.es>
2021-01-09 23:23:27 +0100

    CSS Menu OK

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
 import MenuDatos from './MenuDatos';
+import {Navbar, Nav, Container} from 'react-bootstrap';
 
 // Creacion basica de un componente de prueba
 class Menu extends Component {
     render(){
         return(
-            <div className="menu">
-                <ul>
-                {MenuDatos.map((item, i) => {
-                    return(
-                        <li key={i}>
-                            <Link to={item.path}>{item.titulo}</Link>
-                        </li>
-                    );
-                })}
-                </ul>
-            </div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="/">UjaPack</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                    {MenuDatos.map((item, i) => {
+                        return(
+                            <Nav.Link href={item.path}>
+                                {item.titulo}
+                            </Nav.Link>
+                        );
+                    })}
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
         );
     }
 }
